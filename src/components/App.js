5ad56a355831cb3e94da0b39c5014d86454e7e2e
Karolina Zielińska <hejtulola@outlook.com>
2023-06-19 11:15:34 +0200

    deployed website

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="app">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FiAlignJustify } from 'react-icons/fi';
+import logo from '../images/logo.png';
+
+const Main = () => {
+	return (
+		<div className='main'>
+			<div className='navigation'>
+				<FiAlignJustify
+					style={{
+						fontSize: 30,
+                        color: 'grey',
+					}}
+				/>
+			</div>
+			<div className='main-view'>
+				<img src={logo} alt='' />
+				<div className='text'>
+					<h1>Justyna Kempińska</h1>
+					<p>FRYZJERNIA</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
